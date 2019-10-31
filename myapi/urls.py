c54a8b9cfa13e37266fa3841a6308754083901d5
park sang hyeop <mon2210@naver.com>
2019-10-31 19:40:52 +0900

    apiproject

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,12 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from myapi import views
-
+from rest_framework import urls
 router = DefaultRouter()
 router.register('essay', views.PostViewSet)
 router.register('album', views.ImgViewSet)
 router.register('files', views.FileViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
--- myapi/views.py
+++ myapi/views.py
@@ -6,14 +6,37 @@ from .serializers import EssaySerializer, AlbumSerializer, FilesSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+
+
+
+class Mypagination(PageNumberPagination):
+    page_size = 1
 
 class PostViewSet(viewsets.ModelViewSet):
 
     queryset = Essay.objects.all()
     serializer_class = EssaySerializer
+    pagination_class = Mypagination
+
+    filter_backends = [SearchFilter]
+    search_fields = ('title','body')
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if self.request.user.is_authenticated:
+            if self.request.user.is_staff:
+                pass
+            else:
+                qs = qs.filter(author = self.request.user)
+        else:
+            qs = qs.none()
+        return qs
 
 class ImgViewSet(viewsets.ModelViewSet):
     
@@ -41,3 +64,7 @@ class FileViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=HTTP_201_CREATED)
         else:
             return Response(serializer.error, status=HTTP_400_BAD_REQUEST)
+
+# class PostViewSet(viewsets.ModelViewSet):
+#     queryset = Essay.objects.all()
+#     serializer_class = EssaySerializer
